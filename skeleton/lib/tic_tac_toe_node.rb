f989b912660dd57b3bbb81c4cff98b99b2763c1a
Cassandra McClure <uncertainkitten@gmail.com>
2018-08-24 16:52:44 -0700

    Create #children method in TictactoeNode

--- skeleton/lib/tic_tac_toe_node.rb
+++ skeleton/lib/tic_tac_toe_node.rb
@@ -17,5 +17,23 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children_moves = []
+    (0..2).each do |x|
+      (0..2).each do |y|
+        if @board[[x,y]] == nil
+          new_board = @board.dup
+          new_board[[x,y]] = @next_mover_mark
+          current_mark = @next_mover_mark
+          if @next_mover_mark == :o
+            current_mark = :x
+          else
+            current_mark = :o
+          end
+          new_node = TicTacToeNode.new(new_board, current_mark, [x,y])
+          children_moves << new_node
+        end
+      end
+    end
+    return children_moves
   end
 end
