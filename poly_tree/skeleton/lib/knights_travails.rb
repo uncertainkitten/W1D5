651043d4388e80ecc066810f02ed5aa8875fa04c
Cassandra McClure <uncertainkitten@gmail.com>
2018-08-24 14:54:48 -0700

    Add KnightPathFinder#new_move_positions to create legal move array

--- poly_tree/skeleton/lib/knights_travails.rb
+++ poly_tree/skeleton/lib/knights_travails.rb
@@ -1,7 +1,8 @@
+require_relative "00_tree_node.rb"
 class KnightPathFinder
   attr_reader :root_node
   attr_accessor :visited_positions
-  
+
   def initialize(starting_pos)
     @root_node = PolyTreeNode.new(starting_pos)
     @visited_positions = [starting_pos]
@@ -11,11 +12,16 @@ class KnightPathFinder
   end
 
   def new_move_positions
+    x,y = @root_node.value
+    possible_moves = [[x+2,y+1],[x+2,y-1],[x-2,y+1],[x-2,y-1],[x+1,y-2],[x+1,y+2],[x-1,y-2],[x-1,y+2]]
+    filtered_moves = possible_moves.select {|move| KnightPathFinder.valid_moves(move)}
+    filtered_moves.reject! { |f_move| @visited_positions.include?(f_move) }
+    @visited_positions += filtered_moves
+    filtered_moves
   end
 
   def self.valid_moves(pos)
     pos.each { |v| return false if v > 7 || v < 0}
-    !self.visited_positions.include?(pos)
   end
 
   def find_path(end_pos)
